fed7392e6a23199ee7ef7e493e3c8331e2a67828
Jack Thompson <jacktinternetboy@gmail.com>
2016-07-16 14:53:48 -0400

    test

--- src/init.ts
+++ src/init.ts
@@ -1,9 +1,12 @@
+if (!gpio) {
+    gpio = require("pi-gpio");
+}
 
 function flash() {
     gpio.open(16, "output", function(err) {		// Open pin 16 for output 
         gpio.write(16, 1, function() {			// Set pin 16 high (1) 
             setTimeout(() => {
-                gpio.close(16);
+                gpio.close(16)
                 flash();
             }, 2000);
         });
